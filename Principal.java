881f4f1b74fef222ed8e68816ab6dbd9d0ccddc7
liviacastro <martins.livia05@aluno.ifce.edu.br>
2026-08-18 08:11:58 -0300

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,28 @@
+package br.edu.principal;
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		String nome;
+		double n1, n2, media;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite seu nome: ");
+		nome = sc.next(); 
+		
+		System.out.println("Digite sua nota 01: ");
+		n1 = sc.nextDouble();
+		
+		System.out.println("Digite sua nota 02: ");
+		n2 = sc.nextDouble();
+		
+		media = (n1 + n2)/2;
+		
+		System.out.println("Nome: " + nome);
+		System.out.println("Média: " + media);
+	}
+
+}
